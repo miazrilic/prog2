f2e74cf050ee26c857c633078188ee879559f4a5
miazrilic <mia.zrilic@gmail.com>
2024-06-27 10:52:52 +0200

    Add files via upload

--- prvi.c
+++ prvi.c
@@ -1,88 +1,79 @@
-/* Deklarirati strukturu nogomet u koju će se spremiti ovakvi ulazni 
-podaci: naziv prve momcadi, naziv druge momcadi , golovi_1, golovi_2, datum.
-Nazivi momčadi se mogu sastojati od više riječi, dok varijabla golovi_1 predstavlja broj golova
-koje je zabila prva momčad, a dok varijabla golovi_2 predstavlja broj golova koje je zabila
-druga momčad i može biti samo cijeli broj.
-U program se prvo unosi broj utakmica n (0<n<100).
-S tipkovnice unijeti sve podatke za svaki od n utakmica. 
-	Primjer ulaznih podataka
-	 2
-	 Njemacka
-	 Skotska
-	 2
-	 0
-	 14.06.2024.
-	 Spanjolska
-	 Hrvatska
-	 1
-	 2
-	 15.06.2024.
-    
-Nakon ključne riječi REZULTATI: ispisati sve pobjednike u odigranim utakmicama tijekom 6. mjeseca.
-Primjer ispisa:
+/* Strukture su deklarirane te ih ne trebate mijenjati već samo koristiti u svom programu.
+
+Tijek programa je slijedeći:
+ - korisnik unosi sve podatke za jednu aktivnost (lokaciju, datum, opis i cijenu)
+ - nakon toga, korisnik bira da li želi unijeti još jednu aktivnost. 
+ - Ako unese na tipkovnici 'y', onda unosi podatke za novu aktivnost. Za bilo koji drugi unos, prekida se unos aktivnosti.
+ - korisnik može unijeti maksimalno 10 aktivnosti
+ - po završetku unosa ispisati poruku o broju aktivnosti i ukupnom budgetu za njih u obliku:
+
 	REZULTATI:  
-    Njemacka
-    Spanjolska
+    Broj aktivnosti je: 3
+    Potrositi cu: 20.34 eura
+    
+    Primjer test case-a:
+        Vodice
+        30.06.2024.
+        Koncert omiljenog benda
+        50.74
+        y
+        Vodice
+        1.7.2024.
+        Rent a boat
+        100.50
+        n
+        REZULTATI:
+        Broj aktivnosti je: 2
+        Potrositi cu: 151.24 eura/
+
     */
-    #include<stdio.h>
-    #include<stdlib.h>
-    typedef struct Datum{
-        int dan;
-        int mjesec;
-        int godina;
-    }DATUM;
-    typedef struct Nogomet{
-        char tim1[50];
-        char tim2[50];
-        int golovi_1;
-        int golovi_2;
-        DATUM datum;
-        
-    }NOGOMET;
-    int main(){
-        
-        
-         int n;
-         do{
-            scanf("%d",&n);
-        }while(n<=0 ||  n>=100);
-        NOGOMET* turnir=(NOGOMET*)malloc(n*sizeof(NOGOMET)); 
-        if(turnir==NULL){
-            return 1;
+    
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_ACTIVITIES 10
+#define MAX_LOCATION_LENGTH 50
+
+
+struct Activity {
+    char location[MAX_LOCATION_LENGTH];
+    char date[11]; 
+    char description[100];
+    float budget;  
+};
+
+struct SummerHoliday {
+    struct Activity activities[MAX_ACTIVITIES];
+    int numActivities;
+};
+
+
+int main(){
+        float totalBudget=0;
+        char charY;
+    struct SummerHoliday holiday;
+    holiday.numActivities=0;
+    
+    do{
+        if(holiday.numActivities>=MAX_ACTIVITIES){
+            break;
         }
+        getchar();
+        scanf(" %[^\n]",holiday.activities[holiday.numActivities].location);
+        getchar();
+        scanf(" %[^\n]",holiday.activities[holiday.numActivities].date);
+        getchar();
+        scanf(" %[^\n]",holiday.activities[holiday.numActivities].description);
         
+        scanf("%f", &holiday.activities[holiday.numActivities].budget);
+        totalBudget+=holiday.activities[holiday.numActivities].budget;
+        holiday.numActivities++;
+        getchar();
+        scanf("%c", &charY);
         
-        
-        for(int i=0;i<n;i++){
-            getchar();
-            scanf(" %[^\n]",turnir[i].tim1);
-            getchar();
-            scanf(" %[^\n]",turnir[i].tim2);
-            
-            scanf("%d",&turnir[i].golovi_1);
-            scanf("%d",&turnir[i].golovi_2);
-            scanf("%d.%d.%d",&turnir[i].datum.dan,&turnir[i].datum.mjesec,&turnir[i].datum.godina);
-            
-        }
-       
-       printf("REZULTATI:\n");
-       for(int i=0;i<n;i++){
-           if(turnir[i].datum.mjesec==6 || turnir[i].datum.mjesec==06){
-               if(turnir[i].golovi_1<turnir[i].golovi_2){
-                   printf("%s\n",turnir[i].tim2);
-               }
-               else if(turnir[i].golovi_1>turnir[i].golovi_2){
-                   printf("%s\n",turnir[i].tim1);
-               }
-               
-               
-           }
-               
-               
-           }
-           
-           
-       
-       free(turnir);
-       return 0;
-    }
+    }while(charY=='y');
+    printf("REZULTATI:\n");
+    printf("Broj aktivnosti je: %d\n",holiday.numActivities);
+    printf("Potrositi cu: %.2f eura",totalBudget);
+    return 0;
+}
