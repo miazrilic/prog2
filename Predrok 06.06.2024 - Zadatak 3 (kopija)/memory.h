bd346a722e92fde3088f44cbc193f857d63a56a0
miazrilic <mia.zrilic@gmail.com>
2024-07-02 21:17:48 +0200

    Delete Predrok 06.06.2024 - Zadatak 3 (kopija) directory

--- Predrok 06.06.2024 - Zadatak 3 (kopija)/memory.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEMORY_H
-#define MEMORY_H
-
-void* allocateMemory(size_t, size_t);
-char* strDuplicate(const char*);
-void deallocateMemory(bill_t*);
-void freeMemory(void*);
-
-#endif //MEMORY_H
